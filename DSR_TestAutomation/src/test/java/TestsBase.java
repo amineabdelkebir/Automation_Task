09e593d210627db0e76a81f3e4ecebdb19b988de
amineabdelkebir <abdelkebir.amine89@gmail.com>
2023-11-19 14:43:58 +0000

    Refactor testsbase / add wit mechanism / create execution file to run test and generate allure report

--- DSR_TestAutomation/src/test/java/TestsBase.java
+++ DSR_TestAutomation/src/test/java/TestsBase.java
@@ -1,13 +1,20 @@
+package testutilies;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
 import java.util.concurrent.TimeUnit;
 
-public class TestsBase {
-    public String baseURL = ConfigurationReader.getProperty("baseURL");
+
+public class TestsBase extends testData {
+    public String baseURL = testData.URL;
     public WebDriver driver;
+
     @Parameters({"browser"})
     @BeforeSuite(alwaysRun = true)
     public void driverSetup(@Optional("chrome") String browser) {
@@ -15,11 +22,15 @@ public class TestsBase {
         driver = WebDriverManager.chromedriver().create();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(120,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
         driver.navigate().to(baseURL);
+
+
     }
 
-   @AfterSuite
+    @AfterSuite
     void teardown() {
         driver.quit();
-    }}
+    }
+}
